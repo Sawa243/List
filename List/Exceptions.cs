e75f14dd52affea4d914daf09d9a18c9d1cdb3e4
Alex <bichcov@mal.ru>
2021-06-09 12:53:44 +0300

    AddClassExceptions

---
+++ List/Exceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace List
+{
+    public class Exceptions
+    {
+        public static void CheckExceptionIndex(int index, int length)
+        {
+            if (index < 0 || index > length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public static void CheckNullReferenceException()
+        {
+            throw new NullReferenceException();
+        }
+
+        public static void CheckNullReferenceException(int length)
+        {
+            if (length <= 0)
+            {
+                throw new NullReferenceException();
+            }
+        }
+        public static void CheckExceptionByCountToRemove(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentException("нельзя удалить отрицательное количество элементов");
+            }
+        }
+        public static void CheckExceptionByCountToRemoveInLast(int length, int qty)
+        {
+            if (length < qty)
+            {
+                throw new ArgumentException("Вы пытаетесь удалить, больше элементов, чем их есть");
+            }
+        }
+    }
+}
+}
