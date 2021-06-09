d6c51374659166bb1e56611148d1760449f6082f
Alex <bichcov@mal.ru>
2021-06-09 13:00:36 +0300

    AddLinkedList

---
+++ List/Node/Link.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace List.Node
+{
+    public class Link
+    {
+        public int Value { get; set; }
+        public Link LinkNext { get; set; }
+
+        public Link(int value)
+        {
+            Value = value;
+            LinkNext = null;
+        }
+
+        public Link()
+        {
+            LinkNext = null;
+        }
+    }
+}
